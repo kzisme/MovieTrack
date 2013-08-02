96cbd3ea2e840015d5322a380d7c833f5e05f67c
Kevin Zita <kevin@kevin-ubuntu.(none)>
2013-08-02 01:45:54 -0400

    Test commit

--- MovieTrack.py
+++ MovieTrack.py
@@ -1,9 +1,22 @@
 import sys
+import sqlite3
+import datetime 
+
+
+createDb = sqlite3.connect(':memory:')
+# memory is a placeholder currently
+
+
 
 if len(sys.argv) == 1:
     #We are in the interpreter mode.
     print "We are in the interpreter mode."
     #TODO:All of the code here will do the interpreting.
+
+movie = raw_input('Enter a movie title: ')
+date = raw_input('Enter a date d/m/y')
+rate = raw_input('Please enter a number 1-10')
+
 
 else:
     print "We are in the command line mode."
