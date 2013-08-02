941ea75a24d2c5aef009f1cbd8ffcf8fa2fb8ca9
Kevin Zita <kevin@kevin-ubuntu.(none)>
2013-08-02 02:03:50 -0400

    Fixing stuffs

--- MovieTrack.py
+++ MovieTrack.py
@@ -13,10 +13,10 @@ if len(sys.argv) == 1:
     print "We are in the interpreter mode."
     #TODO:All of the code here will do the interpreting.
 
-movie = raw_input('Enter a movie title: ')
-date = raw_input('Enter a date d/m/y')
-rate = raw_input('Please enter a number 1-10')
-
+    movie = raw_input('Enter a movie title: ')
+    date = raw_input('Enter a date d/m/y':)
+    rate = raw_input('Please enter a number 1-10':)
+    # For an IF statement to run indentation matters.
 
 else:
     print "We are in the command line mode."
