4b80036a1d8594867d0907bc3afafed6d71ba8ad
Kevin Zita <kevin@kevin-ubuntu.(none)>
2013-08-02 00:13:39 -0400

    This is my first code commit

---
+++ MovieTrack.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) == 1:
+    #We are in the interpreter mode.
+    print "We are in the interpreter mode."
+    #TODO:All of the code here will do the interpreting.
+
+else:
+    print "We are in the command line mode."
+    #TODO:All of the code here to parse the arguments.
