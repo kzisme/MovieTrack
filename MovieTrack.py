9d6bfce03d20d721b22f5404c922eef44b713e0f
Kevin Zita <kzita@kent.edu>
2013-12-06 21:53:05 -0500

    Added db insertion - still working

--- MovieTrack.py
+++ MovieTrack.py
@@ -7,22 +7,27 @@ import datetime
 createDb = sqlite3.connect('moviedb.db')
 conn = sqlite3.connect('moviedb.db')
 c = conn.cursor()
-c.execute("DROP TABLE IF EXISTS myMovies")
-c.execute('''create table myMovies
-(movie text, date text, rating text)''')
+#c.execute("DROP TABLE IF EXISTS myMovies")
+c.execute('''CREATE TABLE IF NOT EXISTS myMovies
+(movie text, day text, rating integer)''')
 
+movie = raw_input('Enter a movie title:')
+day = raw_input('Enter a date d/m/y:')
+rating = raw_input('Please enter a number 1-10:')
+
+c.execute("insert into myMovies (movie, day, rating) values (?, ?, ?)",
+			(movie, day, rating))
 #conn.commit() saves changes at the end
+
+'''
 if len(sys.argv) == 1:
     #We are in the interpreter mode.
     print "We are in the interpreter mode."
     #TODO:All of the code here will do the interpreting.
 
-    movie = raw_input('Enter a movie title:')
-    date = raw_input('Enter a date d/m/y:')
-    rating = raw_input('Please enter a number 1-10:')
+
     # For an IF statement to run indentation matters.
-#Here is a comment to test commit
-#Here is a commit test
 else:
     print "We are in the command line mode."
     #TODO:All of the code here to parse the arguments.
+'''
